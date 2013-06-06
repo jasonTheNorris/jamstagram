f89f75d7c607a105f89f361c8f2b80e57792dc27
Jason Norris <jason.norris@rd.io>
2013-06-06 01:14:31 -0700

    Comment out db stuff for now

--- models/models.rb
+++ models/models.rb
@@ -1,23 +1,23 @@
-require 'data_mapper'
+# require 'data_mapper'
 
-class User
-  include DataMapper::Resource
+# class User
+#   include DataMapper::Resource
 
-  property :id, Serial
-  property :name, String
+#   property :id, Serial
+#   property :name, String
 
-  has n, :jamstagrams
-end
+#   has n, :jamstagrams
+# end
 
-class Jamstagram
-  include DataMapper::Resource
+# class Jamstagram
+#   include DataMapper::Resource
 
-  property :id, Serial
-  property :src, String
-  property :title, String
+#   property :id, Serial
+#   property :src, String
+#   property :title, String
 
-  belongs_to :user
-end
+#   belongs_to :user
+# end
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://postgres:postgres@localhost/jamstagram')
-DataMapper.finalize.auto_upgrade!
+# DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://postgres:postgres@localhost/jamstagram')
+# DataMapper.finalize.auto_upgrade!
