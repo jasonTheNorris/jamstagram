18082e5b024ce488f390f9e96d4ca6c3935dc9e1
Jason Norris <jason.norris@rd.io>
2013-06-06 01:01:15 -0700

    Simple Instagram search.

--- public/js/app.js
+++ public/js/app.js
@@ -5,14 +5,28 @@
     Utils: {},
     Models: {},
     Views: {},
-    INSTAGRAM_KEY: 'a8d683aca6dd4263b5bc0bf04ba5d6ec'
+    INSTAGRAM_BASE_URL: 'https://api.instagram.com/v1',
+    INSTAGRAM_CLIENT_ID: 'a8d683aca6dd4263b5bc0bf04ba5d6ec'
   };
 
   // Utils
-  J.Utils.getQueryString = function(args) {
-    return _.map(args, function(v, k) {
+  J.Utils.buildQueryString = function(args) {
+    return '?' + _.map(args, function(v, k) {
       return encodeURIComponent(k) + '=' + encodeURIComponent(v);
     }).join('&');
+  };
+
+  J.Utils.instagramRequest = function(path, args, method) {
+    var args = _.defaults({
+      client_id: J.INSTAGRAM_CLIENT_ID
+    }, args);
+    var url = J.INSTAGRAM_BASE_URL + path + J.Utils.buildQueryString(args);
+
+    return $.ajax({
+      type: method || 'get',
+      dataType: 'jsonp',
+      url: url
+    });
   };
 
   // Models
@@ -33,11 +47,73 @@
 
   J.Views.Create = Backbone.View.extend({
     className: 'Create clearfix',
+
+    events: {
+      'keyup .instagram-search': 'onInstagramSearchKepUp',
+      'click .instagram-results img': 'onInstagramResultClicked',
+      'click .photo-shield': 'onPhotoShieldClicked'
+    },
+
     render: function() {
       J.app.setHeaderButtonState('remove');
       this.$el.html(J.Templates.create.render({ songsLeftText: '5 songs left' }));
       return this;
-    } 
+    },
+
+    onInstagramSearchKepUp: function(e) {
+      if (e.keyCode !== 13) {
+        return;
+      }
+
+      var self = this;
+      var query = $(e.currentTarget).val();
+
+      if (query.charAt(0) === '@') {
+        console.error('IMPLEMENT INSTAGRAM AUTHENTICATION, DUMMY!')
+        // request = J.Utils.instagramRequest('/users/search/', {
+        //   q: query.slice(1)
+        // });
+        // request.success(function(response) {
+        //   if (response.data) {
+        //     var userId = response.data[0].id;
+        //     path = '/users/' + userId + '/media/recent/';
+
+        //     request = J.Utils.instagramRequest(path);
+        //     request.success(function(response) {
+        //       if (response.data) {
+        //         console.log(response.data);
+        //       }
+        //     });
+        //   }
+        // });
+      } else {
+        var path = '/tags/' + query.replace(/^#/, '') + '/media/recent';
+        J.Utils.instagramRequest(path).success(function(response) {
+          var data = _.map(response.data, function(photo) {
+            return {
+              src: photo.images.thumbnail.url,
+              big_src: photo.images.standard_resolution.url,
+              id: photo.id
+            };
+          });
+          var photoGrid = J.Templates.photoGrid.render({ photos: data });
+          self.$('.instagram-results').html(photoGrid);
+        });
+      }
+    },
+
+    onInstagramResultClicked: function(e) {
+      var src = $(e.currentTarget).attr('data-big-src');
+      var $shield = this.$('.photo-shield');
+      $shield.one('load', function() {
+        $shield.fadeIn();
+      });
+      $shield.attr('src', src);
+    },
+
+    onPhotoShieldClicked: function(e) {
+      $(e.currentTarget).fadeOut();
+    }
   });
 
   // App
